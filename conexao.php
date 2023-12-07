cc9a715056e8d7d5ab385895cac5191b3a88bfb9
Pedro Henrique <105438154+ppedrogamess@users.noreply.github.com>
2023-12-07 14:20:07 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -2,10 +2,10 @@
     
     date_default_timezone_set("America/Recife");
 
-    $host = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $db   = 'cs2';
+    $host = 'bu8izlv3hyn53v9jzdgn-mysql.services.clever-cloud.com';
+    $user = 'uk8hkbtaqub0e3my';
+    $pass = 'UCKxO3HKpcFUVxsnsUVZ';
+    $db   = 'bu8izlv3hyn53v9jzdgn';
 
     $conn = mysqli_connect($host, $user, $pass, $db, 3306);
 ?>
